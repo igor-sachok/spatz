b706190813390fa05cc5a4f713dbd5e8b73b0adf
Ihor Sachok (ihor.sachok@unibo.it) <isachok@lagrev5.ee.ethz.ch>
2026-07-31 18:19:08 +0200

    made vl computed from the input vsew for masked instructions

--- hw/ip/spatz/src/spatz_controller.sv
+++ hw/ip/spatz/src/spatz_controller.sv
@@ -762,8 +762,7 @@ module spatz_controller
 
           if (spatz_req.op inside {VMAND, VMANDNOT, VMOR, VMXOR,
                                    VMORNOT, VMNAND, VMNOR, VMXNOR}) begin
-            spatz_req.vtype.vsew = EW_8;
-            spatz_req.vl         = (vl_q + 7) >> 3;
+                spatz_req.vl     = (vl_q + (8 << int'(spatz_req.vtype.vsew)) - 1) >> (int'(spatz_req.vtype.vsew) + 3);
             spatz_req.vstart     = '0;
           end
 
